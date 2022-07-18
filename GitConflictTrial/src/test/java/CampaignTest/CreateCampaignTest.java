714ae31b56d415a1caa837bf2520bb0124ee792f
hp <hp@DESKTOP-MCTOCK3>
2022-07-18 11:47:29 +0530

    Changement

--- GitConflictTrial/src/test/java/CampaignTest/CreateCampaignTest.java
+++ GitConflictTrial/src/test/java/CampaignTest/CreateCampaignTest.java
@@ -7,16 +7,19 @@ public class CreateCampaignTest {
 	@Test
 	public void createCampaignTest() {
 		System.out.println("Create Campaign");
+		System.out.println("Linux");
 	}
 	
 	@Test
 	public void createCampaignWithProduct() {
 		System.out.println("Create Campaign with Product Name");
+System.out.println("Linux");
 	}
 	
 	@Test
 	public void delete() {
 		System.out.println("delete is done ");
+	System.out.println("Red Hat Linux");
 	}
 
 }
